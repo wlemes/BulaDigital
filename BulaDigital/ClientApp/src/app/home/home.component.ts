edac7c2425b7dd927e14801349ac2e5610dc2259
wlemes@fazenda <wlemes@fazenda.ms.gov.br>
2022-05-31 16:50:52 -0400

    edit

--- BulaDigital/ClientApp/src/app/home/home.component.ts
+++ BulaDigital/ClientApp/src/app/home/home.component.ts
@@ -11,15 +11,10 @@ export class HomeComponent {
     public pesquisa: string;
 
     constructor(http: HttpClient, @Inject('BASE_URL') baseUrl: string) {
-        //http.get<Bula>('https://bula.vercel.app/pesquisar?nome=AMOXICILINA&pagina=1').subscribe(result => {
-        //    this.bulas = result;
-        //    console.log('result', result);
-        //}, error => console.error(error));
         this.http = http;
     }
 
     pesquisar() {
-
         console.log("pesquisar ", this.pesquisa);
         this.http.get<Bula>(`https://bula.vercel.app/pesquisar?nome=${this.pesquisa}&pagina=1`).subscribe(result => {
             this.bulas = result.content;
